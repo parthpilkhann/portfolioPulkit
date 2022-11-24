ed0d683e370aaa9dc5b13e905904542687d3a409
parthpilkhann <parthpilkhann@gmail.com>
2022-11-24 18:57:00 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import MainPage from "./pages/MainPage";
 
+
 function App() {
+
   return (
-    <div className="App">
       <MainPage />
-    </div>
   );
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,42 +1,34 @@
-import React, { useEffect } from 'react'
 import "./mainPage.css";
-
+import { Parallax } from "react-parallax";
+import Bg1 from "../img/bg1.jpg"
+import Bg2 from "../img/bg2.jpg"
+import Bg3 from "../img/bg3.jpg"
 
 export default function MainPage() {
-  const handleScroll = () => {
-    let offsetY = window.scrollY
-    console.log(offsetY);
-  }
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
-    return ()=>{
-      window.removeEventListener("scroll", handleScroll)
-    }
-  },[]);
-
-
 
   return (
     <div className='container'>
-      <div className="intro">
-        <div className="topbar">
-          <div className="btnContainer">
-            <button className='btn'>HOME</button>
-            <button className='btn'>THE TOMAR'S</button>
-            <button className='btn'>LATEST WORK</button>
-            <button className='btn'>THE BLOG</button>
-            <button className='btn'>BOOK NOW</button>
+      <Parallax strength={200} bgImage={Bg1} >
+        <div className="intro">
+          <div className="topbar">
+            {/* <div className="btnContainer">
+              <button className='btn'>HOME</button>
+              <button className='btn'>THE TOMAR'S</button>
+              <button className='btn'>LATEST WORK</button>
+              <button className='btn'>THE BLOG</button>
+              <button className='btn'>BOOK NOW</button>
+            </div> */}
+          </div>
+          <div className="name">
+            <p className='booking'>BOOKING EVERYWHERE</p>
+            <h1 className='brandName'>PULKIT TOMAR</h1>
+            <h4 className='ownersName' >BY TOMAR'S</h4>
+          </div>
+          <div className='downArrowContainer'>
+            <i className="fa-solid fa-chevron-down arrow"></i>
           </div>
         </div>
-        <div className="name">
-          <p className='booking'>BOOKING EVERYWHERE</p>
-          <h1 className='brandName'>PULKIT TOMAR</h1>
-          <h4 className='ownersName' >BY TOMAR'S</h4>
-        </div>
-        <div className='downArrowContainer'>
-          <i className="fa-solid fa-chevron-down arrow"></i>
-        </div>
-      </div>
+      </Parallax>
 
       <div className="whatWeDo1">
         <div className="upperContainer">
@@ -48,21 +40,29 @@ export default function MainPage() {
           <div className="leftContainer">
             <p className='leftTxt' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, veritatis. Natus sed quod quia laudantium?</p>
             <img className='leftImg1' src={require("../img/l1.jpg")} alt="not found" />
+            {/* <Breakpoint m down >
+              <img className='rightImg2' src={require("../img/r1.jpg")} alt="not found" />
+              <img className='rightImg2' src={require("../img/r2.jpg")} alt="not found" />
+            </Breakpoint> */}
           </div>
+          {/* <Breakpoint m up> */}
           <div className="rightContainer">
             <img className='rightImg2' src={require("../img/r1.jpg")} alt="not found" />
             <img className='rightImg2' src={require("../img/r2.jpg")} alt="not found" />
             <img className='rightImg3' src={require("../img/r3.jpg")} alt="not found" />
           </div>
+          {/* </Breakpoint> */}
         </div>
       </div>
 
-      <div className="whatWeDo2">
-        <div>
-          <h1>FOR THOSE IN LOVE + BOLDLY DRESSED</h1>
-          <h6>YOU ARE UNIQUE AND YOUR PHOTOS SHOULD REFLECT THAT</h6>
+      <Parallax strength={200} bgImage={Bg2} >
+        <div className="whatWeDo2">
+          <div>
+            <h1>FOR THOSE IN LOVE + BOLDLY DRESSED</h1>
+            <h6>YOU ARE UNIQUE AND YOUR PHOTOS SHOULD REFLECT THAT</h6>
+          </div>
         </div>
-      </div>
+      </Parallax>
 
       <div className="about">
         <div className="headingContainer">
@@ -83,19 +83,30 @@ export default function MainPage() {
         </div>
       </div>
 
-      <div className="featured">
-        <div className="featuredTxt">
-          <h1>RECENTLY FEATURED IN</h1>
-          <p>Photovogue by Vogue Italia , International Rare Disease Film Festival – Berlin , Young Uttarakhand Cine Awards</p>
+      <Parallax strength={200} bgImage={Bg3} >
+        <div className="featured">
+          <div className="featuredTxt">
+            <h1>RECENTLY FEATURED IN</h1>
+            <p>Photovogue by Vogue Italia , International Rare Disease Film Festival – Berlin , Young Uttarakhand Cine Awards</p>
+          </div>
         </div>
-      </div>
+      </Parallax>
 
       <div className="contactContainer">
         <div className="logos">
-          <i className="fa-brands fa-instagram"></i>
-          <i className="fa-brands fa-facebook-f"></i>
-          <i className="fa-brands fa-pinterest-p"></i>
+          <a href="http://" target={"_blank"} >
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="http://" target={"_blank"} >
+            <i className="fa-brands fa-facebook-f"></i>
+          </a>
+          <a href="http://" target={"_blank"} >
+            <i className="fa-brands fa-pinterest-p"></i>
+          </a>
         </div>
+      </div>
+      <div className="contactDetails">
+        <p>pulkittomar@gmail.com | <a href="http://creatingtales.com">www.creatingtales.com</a></p>
       </div>
 
     </div>
